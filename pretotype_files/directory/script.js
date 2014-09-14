83fd7a929479164dd41827927e3161c64446a55a
Alex Song <ss456@duke.edu>
2014-09-14 16:28:21 -0400

    added basic layout for doctor directory

---
+++ pretotype_files/directory/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('.entry').mouseover(function() {
+	    $(this).css({'background-color':'#e7e7e7'});
+	}).mouseout(function() {
+		$(this).css({'background-color':'#ffffff'});
+	});
+});
