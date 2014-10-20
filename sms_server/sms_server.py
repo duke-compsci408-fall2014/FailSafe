810f9dc3f83cc64f5be3a13d930673fe7c9abc58
Jeff Hou <j.king.hou@gmail.com>
2014-10-20 23:31:30 +0000

    Created loop for communication methods in call_server.py. sms_server now deprecated

--- sms_server/sms_server.py
+++ sms_server/sms_server.py
@@ -59,53 +59,5 @@ def test_prep_template_sms():
     r = requests.post("http://colab-sbx-131.oit.duke.edu:5001/test_send_template_sms", params=data)
     return "yay"
 
-@app.route("/handle", methods=['GET', 'POST'])
-def message_handler():
-    """Respond and greet the caller by name."""
-    print("PT A")
-    from_number = request.values.get('From', None)
-    message = str(request.values.get('Body', None)).lower()
-    print(message)
-    print(request.values)
-    if message[:3] == "add" or message[:3] == "set":
-        print("1")
-        if from_number not in callers:
-            callers[from_number] = {}
-        card = message[3:]
-        card_info = card.split(":")
-        callers[from_number][card_info[0]] = card_info[1]
-
-        message = "Added! Front:" + str(card_info[0]) + "\nBack: " +  str(callers[from_number][card_info[0]])
-    elif message[:4] == "stahp":
-        message = "Stopping."
-        callers_on[from_number] = -1
-    elif message[:5] == "clear":
-        callers[from_number]={}
-        message = "Cleared!"
-    elif message[:4] == "quiz" or message[:4] == "test" or callers_on[from_number]>-1:
-        if from_number in callers_on and callers_on[from_number] == 1:
-           if callers_last[from_number] == message:
-               message = "CORRECT! "
-           else:
-               message = "INCORRECT! Back: " + str(callers_last[from_number]) + " "
-        else:
-            message = ""
-        callers_on[from_number] = 1
-        num_of_cards = len(callers[from_number].keys())
-        print(num_of_cards)
-        print(callers[from_number].keys())
-        card=[0,0]
-        index = random.randint(0,num_of_cards-1)
-        card[1] = callers[from_number][callers[from_number].keys()[index]]
-        card[0] = callers[from_number].keys()[index]
-        print(card)
-        message += "Front: " + str(card[0])
-        print(message)
-        callers_last[from_number] = card[1]
-
-    message = client.messages.create(to=from_number, from_="+14138533700",
-                                     body=message)
-    return str(resp)
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5001)
