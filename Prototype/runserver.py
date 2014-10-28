abde2b851ed390ee2d5d426f73d0e99574a510fd
Jeff Hou <j.king.hou@gmail.com>
2014-10-28 01:50:19 +0000

    changed production server back to 5000

--- Prototype/runserver.py
+++ Prototype/runserver.py
@@ -17,4 +17,4 @@ def index():
 if __name__ == '__main__':
     app.debug = True
     app.secret_key = "!$REGG$#GBGGA#!REFGSDFFAFAGRTG%Y$T@#R!@T$%$^T$#!%@RFDSDSB"
-    app.run(host='0.0.0.0', port=7000)
+    app.run(host='0.0.0.0', port=5000)
